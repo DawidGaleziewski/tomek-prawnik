1d4fc0ea5f3b15f72aa4ef1aeb310720a49d3bad
Dawid Gałęziewski <dawid.galeziewski.dev@gmail.com>
2022-03-12 15:14:16 +0100

    update: basic section setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,20 @@
 import React, { FunctionComponent, Fragment } from 'react'
 import { ThemeProvider } from '@emotion/react'
-import 'normalize.css';
-import '../theme/global.css';
-import { LAWTheme, ILAWTheme } from '../theme/law';
+import 'normalize.css'
+import '../theme/global.css'
+import { LAWTheme, ILAWTheme } from '../theme/law'
 // componets
-import {UXContainer} from "../atoms/UXContainer";
-import {Typography, TypographyVariants} from "../atoms/Typography";
-import {Section, SectionVariants} from "../atoms/Section";
-
+import { UXContainer } from '../atoms/UXContainer'
+import { Typography, TypographyVariants } from '../atoms/Typography'
+import { Section, SectionVariants } from '../atoms/Section'
 
 interface IOfferBox {
   headerText: string
   headerImg: string
 }
 
-
 declare module '@emotion/react' {
-  export interface Theme extends ILAWTheme  {}
+  export interface Theme extends ILAWTheme {}
 }
 
 const OfferBox: FunctionComponent<IOfferBox> = (props) => {
@@ -31,159 +29,173 @@ const OfferBox: FunctionComponent<IOfferBox> = (props) => {
   )
 }
 
-
 // markup
 const IndexPage = () => {
   return (
     <ThemeProvider theme={LAWTheme}>
-    <Fragment>
-      <UXContainer>
-        <nav>
-          <ul>
-            <li>O mnie</li>
-            <li>cennik</li>
-            <li>kontakt</li>
-          </ul>
-        </nav>
-      </UXContainer>
+      <Fragment>
+        <UXContainer>
+          <nav>
+            <ul>
+              <li>O mnie</li>
+              <li>cennik</li>
+              <li>kontakt</li>
+            </ul>
+          </nav>
+        </UXContainer>
 
-      <main>
-        <section>
-          <UXContainer>
-            <Typography as="h1">
-              Kancelaria Radcy Prawnego <br />
-              Tomasz Mikusek
-            </Typography>
-          </UXContainer>
-        </section>
-
-        <Section variant={SectionVariants.DARK}>
-          <UXContainer>
-            <Typography as="h2" variant={TypographyVariants.SECTION_HEADER}>Zakres usług</Typography>
-            <div>
-              <OfferBox headerText="Odszkodowania" headerImg="test">
-                <Typography>
-                  Błędy medyczne, wypadki komunikacyjne, katastrofy budowlane,
-                  wypadki przy pracy i inne
-                </Typography>
-              </OfferBox>
-              <OfferBox headerText="Obsługa przedsiębiorców" headerImg="test">
-                <Typography>
-                  Prawo korporacyjne i prawo spółek handlowych
-                </Typography>
-              </OfferBox>
-              <OfferBox headerText="Prawo karne" headerImg="test">
-                <Typography>
-                  Obrońca i pełnomocnik w toku postępowania karnego,
-                  karnoskarbowego i wykroczeniowego Prawo karne gospodarcze
-                </Typography>
-              </OfferBox>
-              <OfferBox headerText="Prawo cywilne" headerImg="test">
-                <Typography>
-                  Zobowiązania, umowy, dochodzenie wierzytelności, prawo
-                  spadkowe
-                </Typography>
-              </OfferBox>
-              <OfferBox headerText="Bieżąca obsługa prawna" headerImg="test">
-                <Typography>
-                  Reprezentacja w toku procesu reprezentacja w toku egzekucji,
-                  sporządzanie opinii prawnych, analiza umów, porady prawne i
-                  negocjacje
-                </Typography>
-              </OfferBox>
-              <OfferBox headerText="Prawo rodzinne" headerImg="test">
-                <Typography>
-                  Rozwód i separacja, władza rodzicielska, ustalenie kontaktów,
-                  podział majątku, alimenty, postępowania w sprawach nieletnich
-                </Typography>
-              </OfferBox>
-            </div>
-          </UXContainer>
-        </Section>
-
-        <section>
-          <UXContainer>
-            <Typography as="h2">Kim jestem?</Typography>
-
-            <div>
-              <Typography as="h2">Tomasz Mikusek</Typography>
-
-              <Typography>
-                Jestem radcą prawnym wpisanym na listę radców prawnych
-                prowadzoną przez Okręgową Izbę Radców Prawnych w Warszawie -
-                wpis nr WA-14592.
+        <main>
+          <section>
+            <UXContainer>
+              <Typography as="h1">
+                Kancelaria Radcy Prawnego <br />
+                Tomasz Mikusek
               </Typography>
-              <Typography>
-                Posiadam wieloletnie doświadczenie zawodowe pozwalające na
-                rzetelne prowadzenie powierzonych mi przez Państwa spraw, a
-                obowiązek doskonalenia zawodowego oraz chęć podnoszenia
-                kwalifikacji i samokształcenia prowadzi do ciągłego podnoszenia
-                poziomu świadczonej obsługi prawnej.
-              </Typography>
-              <Typography>
-                Gwarantuję, iż podjęte przeze mnie z należytą starannością
-                działania, optymalnie zabezpieczają interesy klienta.
-              </Typography>
-            </div>
-            <div>
-              <Typography>
-                Ukończyłem studia prawnicze na Wydziale Prawa i Administracji
-                Uniwersytetu Marii Curie- Skłodowskiej w Lublinie, a następnie
-                aplikację radcowską przy Okręgowej Izbie Radców Prawnych w
-                Warszawie.
-              </Typography>
-              <Typography>
-                Studiowałem również filologię rosyjską (specjalizacja: język
-                rosyjski w biznesie) na Wydziale Humanistycznym Uniwersytetu
-                Marii Curie-Skłodowskiej.
-              </Typography>
-            </div>
-          </UXContainer>
-        </section>
+            </UXContainer>
+          </section>
 
-        <section>
-          <Typography as="h2">Wynagrodzenie</Typography>
+          <Section variant={SectionVariants.DARK}>
+            <UXContainer>
+              <Typography as="h2" variant={TypographyVariants.SECTION_HEADER}>
+                Zakres usług
+              </Typography>
+              <div>
+                <OfferBox headerText="Odszkodowania" headerImg="test">
+                  <Typography>
+                    Błędy medyczne, wypadki komunikacyjne, katastrofy budowlane,
+                    wypadki przy pracy i inne
+                  </Typography>
+                </OfferBox>
+                <OfferBox headerText="Obsługa przedsiębiorców" headerImg="test">
+                  <Typography>
+                    Prawo korporacyjne i prawo spółek handlowych
+                  </Typography>
+                </OfferBox>
+                <OfferBox headerText="Prawo karne" headerImg="test">
+                  <Typography>
+                    Obrońca i pełnomocnik w toku postępowania karnego,
+                    karnoskarbowego i wykroczeniowego Prawo karne gospodarcze
+                  </Typography>
+                </OfferBox>
+                <OfferBox headerText="Prawo cywilne" headerImg="test">
+                  <Typography>
+                    Zobowiązania, umowy, dochodzenie wierzytelności, prawo
+                    spadkowe
+                  </Typography>
+                </OfferBox>
+                <OfferBox headerText="Bieżąca obsługa prawna" headerImg="test">
+                  <Typography>
+                    Reprezentacja w toku procesu reprezentacja w toku egzekucji,
+                    sporządzanie opinii prawnych, analiza umów, porady prawne i
+                    negocjacje
+                  </Typography>
+                </OfferBox>
+                <OfferBox headerText="Prawo rodzinne" headerImg="test">
+                  <Typography>
+                    Rozwód i separacja, władza rodzicielska, ustalenie
+                    kontaktów, podział majątku, alimenty, postępowania w
+                    sprawach nieletnich
+                  </Typography>
+                </OfferBox>
+              </div>
+            </UXContainer>
+          </Section>
 
-          <div>
-            <Typography>
-              Wynagrodzenie zawsze jest ustalane indywidualnie z klientem, w
-              zależności od charakteru prowadzonej sprawy, stopnia jej
-              skomplikowania oraz nakładu pracy - po dokonaniu jej wstępnej
-              analizy.
-            </Typography>
-            <Typography>
-              Z uwagi na powyższe istnieje wiele form dokonywania rozliczeń:
-            </Typography>
-            <Typography>
-              - ryczałt (za konkretne czynności podejmowane w sprawie,
-              prowadzenie danego etapu sprawy bądź za poprowadzenie całej
-              sprawy),
-            </Typography>
-            <Typography>
-              - wynagrodzenie okresowe/abonamentowe (miesięczne) za obsługę
-              prawną (np. przedsiębiorstw) obliczaną stosownie do ustalonej
-              liczby godzin określonej indywidualnie w umowie,
-            </Typography>
-            <Typography>- wynagrodzenie godzinowe,</Typography>
-            <Typography>- wynagrodzenie godzinowe,</Typography>
-            <Typography>
-              -wynagrodzenie, tzw. success fee, tj. prowizyjne.
-            </Typography>
-          </div>
-        </section>
+          <Section variant={SectionVariants.DARK}>
+            <UXContainer>
+              <Typography as="h2" variant={TypographyVariants.SECTION_HEADER}>
+                Kim jestem?
+              </Typography>
 
-        <section>
-          <Typography as="h2">Skontaktuj się</Typography>
-          <form>
-            <div>
-              <label>Imię</label>
-              <input />
-            </div>
-          </form>
-        </section>
-      </main>
-    </Fragment>
-     </ThemeProvider>
+              <div>
+                <Typography as="h2" variant={TypographyVariants.SECTION_HEADER}>
+                  Tomasz Mikusek
+                </Typography>
+
+                <Typography>
+                  Jestem radcą prawnym wpisanym na listę radców prawnych
+                  prowadzoną przez Okręgową Izbę Radców Prawnych w Warszawie -
+                  wpis nr WA-14592.
+                </Typography>
+                <Typography>
+                  Posiadam wieloletnie doświadczenie zawodowe pozwalające na
+                  rzetelne prowadzenie powierzonych mi przez Państwa spraw, a
+                  obowiązek doskonalenia zawodowego oraz chęć podnoszenia
+                  kwalifikacji i samokształcenia prowadzi do ciągłego
+                  podnoszenia poziomu świadczonej obsługi prawnej.
+                </Typography>
+                <Typography>
+                  Gwarantuję, iż podjęte przeze mnie z należytą starannością
+                  działania, optymalnie zabezpieczają interesy klienta.
+                </Typography>
+              </div>
+              <div>
+                <Typography>
+                  Ukończyłem studia prawnicze na Wydziale Prawa i Administracji
+                  Uniwersytetu Marii Curie- Skłodowskiej w Lublinie, a następnie
+                  aplikację radcowską przy Okręgowej Izbie Radców Prawnych w
+                  Warszawie.
+                </Typography>
+                <Typography>
+                  Studiowałem również filologię rosyjską (specjalizacja: język
+                  rosyjski w biznesie) na Wydziale Humanistycznym Uniwersytetu
+                  Marii Curie-Skłodowskiej.
+                </Typography>
+              </div>
+            </UXContainer>
+          </Section>
+
+          <Section variant={SectionVariants.DARK}>
+            <UXContainer>
+              <Typography as="h2" variant={TypographyVariants.SECTION_HEADER}>
+                Wynagrodzenie
+              </Typography>
+
+              <div>
+                <Typography>
+                  Wynagrodzenie zawsze jest ustalane indywidualnie z klientem, w
+                  zależności od charakteru prowadzonej sprawy, stopnia jej
+                  skomplikowania oraz nakładu pracy - po dokonaniu jej wstępnej
+                  analizy.
+                </Typography>
+                <Typography>
+                  Z uwagi na powyższe istnieje wiele form dokonywania rozliczeń:
+                </Typography>
+                <Typography>
+                  - ryczałt (za konkretne czynności podejmowane w sprawie,
+                  prowadzenie danego etapu sprawy bądź za poprowadzenie całej
+                  sprawy),
+                </Typography>
+                <Typography>
+                  - wynagrodzenie okresowe/abonamentowe (miesięczne) za obsługę
+                  prawną (np. przedsiębiorstw) obliczaną stosownie do ustalonej
+                  liczby godzin określonej indywidualnie w umowie,
+                </Typography>
+                <Typography>- wynagrodzenie godzinowe,</Typography>
+                <Typography>- wynagrodzenie godzinowe,</Typography>
+                <Typography>
+                  -wynagrodzenie, tzw. success fee, tj. prowizyjne.
+                </Typography>
+              </div>
+            </UXContainer>
+          </Section>
+
+          <Section variant={SectionVariants.DARK}>
+            <UXContainer>
+              <Typography as="h2" variant={TypographyVariants.SECTION_HEADER}>
+                Skontaktuj się
+              </Typography>
+              <form>
+                <div>
+                  <label>Imię</label>
+                  <input />
+                </div>
+              </form>
+            </UXContainer>
+          </Section>
+        </main>
+      </Fragment>
+    </ThemeProvider>
   )
 }
 
