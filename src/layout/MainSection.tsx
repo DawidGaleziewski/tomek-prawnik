4de161f9b1920dbf81044395f93b34a7cf155fa2
uniters_ngo <uniters-fundation@tutanota.com>
2022-05-28 12:59:38 +0200

    update: changed to new image format

--- src/layout/MainSection.tsx
+++ src/layout/MainSection.tsx
@@ -1,38 +1,35 @@
 import React from 'react'
 
-import { Typography, TypographyVariants } from '../atoms/Typography';
-import styled from '@emotion/styled';
-// import mainSectionBackground from './images/main-image.jpg';
-import mainSectionBackground from './images/main_image_variant_1.jpg';
-// import mainSectionBackground from './images/main-image_variant_2.jpg';
-// import mainSectionBackground from './images/main-image_variant_3.jpg';
+import { Typography, TypographyVariants } from '../atoms/Typography'
+import styled from '@emotion/styled'
 
-import { UXContainer } from '../atoms/UXContainer';
+import mainSectionBackground from './images/main_image.jpg'
+import mainSectionBackgroundMobile from './images/main_image_mobile.png'
 
-
+import { UXContainer } from '../atoms/UXContainer'
 
 export const MainSection = () => (
   <MainSectionBase>
     {/* <Image css={imageBackgroundStyle} src="temida_grey.svg" alt="temida" /> */}
-      <UXContainer>
-        <Typography as="h1" variant={TypographyVariants.MAIN_HEADER}>
-          Kancelaria Radcy Prawnego <br />
-          Tomasz Mikusek
-        </Typography>
-      </UXContainer>      
+    <UXContainer>
+      <Typography as="h1" variant={TypographyVariants.MAIN_HEADER}>
+        Kancelaria Radcy Prawnego <br />
+        Tomasz Mikusek
+      </Typography>
+    </UXContainer>
   </MainSectionBase>
 )
 
-
 const MainSectionBase = styled.section`
-    position: relative;
-    padding: 15rem 4.2rem;
-    background-image: url(${mainSectionBackground});
-    background-size: cover;
-    background-repeat: no-repeat;
-    /* background-position: 50% 50%; */;
+  position: relative;
+  padding: 15rem 4.2rem;
+  background-image: url(${mainSectionBackgroundMobile});
+  background-size: cover;
+  background-repeat: no-repeat;
+  /* background-position: 50% 50%; */
 
-    @media (min-width: ${props => props.theme.breakpoint.medium + 'px'}){
-          padding: 30rem 0;
-    }
-`;
+  @media (min-width: ${(props) => props.theme.breakpoint.medium + 'px'}) {
+    background-image: url(${mainSectionBackground});
+    padding: 30rem 0;
+  }
+`
